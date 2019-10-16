e1e03ef75f485391366fed4d4a0991849ae9a636
Yvone Sanchez <yvone.sre@gmail.com>
2019-10-15 20:41:26 -0500

    adds error handling

--- app.js
+++ app.js
@@ -20,6 +20,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/auth', usersRouter);
 
+// app.use(logErrors)
+app.use(clientErrorHandler)
+app.use(errorHandler)
+
+// function logErrors (err, req, res, next) {
+//   console.error(err.stack)
+//   next(err)
+// }
+
+function clientErrorHandler (err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send({ error: 'Something failed!' })
+  } else {
+    next(err)
+  }
+}
+
+function errorHandler (err, req, res, next) {
+  res.status(err.status || 500).json({ error: err })
+}
+
 app.get('*', (req,res,next) => {
 	res.sendStatus(404);
 })
